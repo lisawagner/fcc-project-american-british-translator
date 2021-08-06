b34a19438e2563342ea8540f6e756e901dc92cb2
Mohamed <mkf6767@gmail.com>
2021-08-06 16:08:25 +0200

    completed 'translator.js' & 'api.js'

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,82 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-class Translator {
+const americanOnlyKeys = Object.keys(americanOnly)
+const americanToBritishSpellingKeys = Object.keys(americanToBritishSpelling)
+const britishToAmericanSpellingValues = Object.values(americanToBritishSpelling)
+const americanToBritishTitlesKeys = Object.keys(americanToBritishTitles)
+const americanToBritishTitlesValues = Object.values(americanToBritishTitles)
+const britishOnlyKeys = Object.keys(britishOnly)
 
+const americanExp = new RegExp(americanToBritishTitlesKeys.reduce((acc, key, index)=>{
+    let result = acc.concat(`(${key})`)
+    if(index !== americanToBritishTitlesKeys.length - 1){
+        result = result.concat("|")
+    }
+    return result
+}, americanToBritishSpellingKeys.reduce((acc, key)=>{
+    return acc.concat(`(${key})|`)
+}, americanOnlyKeys.reduce((acc, key)=>{
+    return acc.concat(`(${key})|`)
+}, ""))), "gi")
+
+const britishExp = new RegExp(americanToBritishTitlesValues.reduce((acc, key, index)=>{
+    let result = acc.concat(`(${key})`)
+    if(index !== americanToBritishTitlesValues.length - 1){
+        result = result.concat("|")
+    }
+    return result
+}, britishToAmericanSpellingValues.reduce((acc, key)=>{
+    return acc.concat(`(${key})|`)
+}, britishOnlyKeys.reduce((acc, key)=>{
+    return acc.concat(`(${key})|`)
+}, ""))), "gi")
+
+function getKeyFromValue(keys, obj, val){
+    return keys.find(key=>obj[key] === val)
+}
+
+class Translator {
+    translate(text, locale){
+        switch(locale){
+            case "american-to-british":
+                let britishResult = text
+                let americanExpResult
+                while((americanExpResult = americanExp.exec(text)) !== null){
+                    const match = americanExpResult[0]
+                    const search = match.toLowerCase()
+                    britishResult = britishResult.replace(match, `<span class="highlight">${americanOnly[search]}</span>` || `<span class="highlight">${americanToBritishSpelling[search]}</span>` || `<span class="highlight">${americanToBritishTitles[search]}</span>`)
+                }
+                const americanTimeExpResult = /\d{1,2}:\d{1,2}/g.exec(britishResult)
+                if(americanTimeExpResult){
+                    const match = americanTimeExpResult[0]
+                    const replaced = match.replace(":", ".")
+                    britishResult = britishResult.replace(match, `<span class="highlight">${replaced}</span>`)
+                }
+                if(britishResult === text){
+                    return "Everything looks good to me!"
+                }
+                return britishResult
+            case "british-to-american":
+                let americanResult = text
+                let britishExpResult
+                while((britishExpResult = britishExp.exec(text)) !== null){
+                    const match = britishExpResult[0]
+                    const search = match.toLowerCase()
+                    americanResult = americanResult.replace(match, `<span class="highlight">${britishOnly[search]}</span>` || `<span class="highlight">${getKeyFromValue(americanToBritishSpellingKeys, americanToBritishSpelling, search)}</span>` || `<span class="highlight">${getKeyFromValue(americanToBritishTitlesKeys, americanToBritishTitles, search)}</span>`)
+                }
+                const britishTimeExpResult = /\d{1,2}.\d{1,2}/g.exec(americanResult)
+                if(britishTimeExpResult){
+                    const match = britishTimeExpResult[0]
+                    const replaced = match.replace(".", ":")
+                    americanResult = americanResult.replace(match, `<span class="highlight">${replaced}</span>`)
+                }
+                if(americanResult === text){
+                    return "Everything looks good to me!"
+                }
+                return americanResult
+        }
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,35 @@ module.exports = function (app) {
   const translator = new Translator();
 
   app.route('/api/translate')
-    .post((req, res) => {
-      
+    .post((req, res, next)=>{
+      const input = req.body
+      if(!input.text || !input.locale){
+        return res.json({
+          error: "Required field(s) missing"
+        })
+      }
+      next()
+    }, (req, res, next) => {
+      const input = req.body
+      if(input.text.length === 0){
+        return res.json({
+          error: "No text to translate"
+        })
+      }
+      next()
+    }, (req, res, next)=>{
+      const input = req.body
+      if(input.locale !== "american-to-british" || input.locale !== "british-to-american"){
+        return res.json({
+          error: "Invalid value for locale field"
+        })
+      }
+      next()
+    }, (req, res)=>{
+      const input = req.body
+      res.json({
+        text: input.text,
+        translation: translator.translate(input.text, input.locale)
+      })
     });
 };
