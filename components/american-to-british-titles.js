da8f1c27ed726ed2a5a2ab94cf7f3eaf3315ce7e
Mohamed <mkf6767@gmail.com>
2021-08-08 15:43:36 +0200

    reorganised some lines

--- components/american-to-british-titles.js
+++ components/american-to-british-titles.js
@@ -1,8 +1,8 @@
 module.exports = {
-  'mrs.': 'mrs',
-  'mr.': 'mr',
-  'ms.': 'ms',
-  'mx.': 'mx',
-  'dr.': 'dr',
-  'prof.': 'prof'
+  'Mrs.': 'Mrs',
+  'Mr.': 'Mr',
+  'Ms.': 'Ms',
+  'Mx.': 'Mx',
+  'Dr.': 'Dr',
+  'Prof.': 'Prof'
 }
